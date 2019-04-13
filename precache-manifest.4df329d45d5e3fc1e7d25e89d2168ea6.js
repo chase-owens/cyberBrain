ede3e58707a1607be2510d5f1216bf5b5e517af9
Chase-Owens <chasejonathanowens@gmail.com>
2019-04-12 19:32:02 -0500

    Updates

---
+++ precache-manifest.4df329d45d5e3fc1e7d25e89d2168ea6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a676a851e35f098b0d50",
+    "url": "/cyberBrain/static/css/main.17a8189c.chunk.css"
+  },
+  {
+    "revision": "a676a851e35f098b0d50",
+    "url": "/cyberBrain/static/js/main.2e71a601.chunk.js"
+  },
+  {
+    "revision": "1d5e11ef9745231ea0d6",
+    "url": "/cyberBrain/static/js/runtime~main.9ca875ba.js"
+  },
+  {
+    "revision": "7ce85f181e3ebc7d63ec",
+    "url": "/cyberBrain/static/js/2.945216f2.chunk.js"
+  },
+  {
+    "revision": "c7ec52575cb3543c23fa8d01f405d317",
+    "url": "/cyberBrain/static/media/birdbrain.c7ec5257.jpg"
+  },
+  {
+    "revision": "4b2884bead3c479f9e18aa85b7a3d24a",
+    "url": "/cyberBrain/static/media/birdbrain3.4b2884be.jpg"
+  },
+  {
+    "revision": "2aa44c05f85fe6709868bba03171f1ed",
+    "url": "/cyberBrain/static/media/birdbrainSkinner.2aa44c05.jpg"
+  },
+  {
+    "revision": "c9849407363f159c01553397acf36b6a",
+    "url": "/cyberBrain/static/media/o.c9849407.png"
+  },
+  {
+    "revision": "38341b85a4f57f04779d7316e649253a",
+    "url": "/cyberBrain/static/media/x.38341b85.png"
+  },
+  {
+    "revision": "34a47fa6a382a1b6c2ad4680ec395747",
+    "url": "/cyberBrain/static/media/BaileyGillaspyFigure5.34a47fa6.jpg"
+  },
+  {
+    "revision": "2751157b5f89cc496de206fbf54c2c85",
+    "url": "/cyberBrain/static/media/birdbrain_1024X768.2751157b.jpg"
+  },
+  {
+    "revision": "6647771488427bfda92bb03257cf74fc",
+    "url": "/cyberBrain/static/media/birdbrain9.66477714.jpg"
+  },
+  {
+    "revision": "b89543da37f0fe74ffb27ae4ddb52ea1",
+    "url": "/cyberBrain/static/media/chicken.b89543da.png"
+  },
+  {
+    "revision": "c3e9ca414509d90495edecfc7a9c7972",
+    "url": "/cyberBrain/index.html"
+  }
+];
